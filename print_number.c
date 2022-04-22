be0d00420d5ad0fa3b8e406a25a48493a503fe1a
kasshunT <ktilahun152@gmail.com>
2022-04-22 02:41:58 -0700

    adding print number to the pritf project

--- print_number.c
+++ print_number.c
@@ -12,7 +12,7 @@ int _isdigit(int c)
 }
 
 /**
- * _strlen - returns the length of a string
+ * _strlen - returns length of a string
  * @s: the string whose length to check
  *
  * Return: integer length of string
